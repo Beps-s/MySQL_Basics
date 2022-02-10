696f9f6ebcc272b548e51ce2b82989f7b6d6c267
UwUBeebi <chris.erman@khk.ee>
2022-02-10 15:19:05 +0200

    Created mysql table

--- demo.js
+++ demo.js
@@ -3,14 +3,16 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Qwerty123!"
+  password: "Qwerty123!",
+  database: "mydb"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
+  var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Database created");
+    console.log("Table created");
   });
 });
